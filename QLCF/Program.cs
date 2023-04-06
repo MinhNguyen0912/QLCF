6cf3ca6275a95d8b1c2eaf7018a2ab1b140c343e
ACER <ACER@NGOCHAI>
2023-04-07 00:45:13 +0700

    a

--- QLCF/Program.cs
+++ QLCF/Program.cs
@@ -16,7 +16,7 @@ namespace QLCF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FQuanLyBan());
+            Application.Run(new fQuanLyKho());
         }
     }
 }
